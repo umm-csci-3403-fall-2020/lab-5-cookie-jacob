92b4773e3cd38f578f273cc77fdfdfe1786e0411
Cookie Vang <vang2660@morris.umn.edu>
2020-10-08 11:25:51 -0500

    Start on setting up socket for client side

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,20 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static final int portNumber = 6013;
+	public static void main(String[] args) throws IOException {
+		String server;
+		if (args.length == 0){
+			server = "127.0.0.1";
+		}
+		else {
+			server = args[0];
+		}
+
+		try {
+			Socket socket = new Socket(server, portNumber);
+		}
+	}
 }
